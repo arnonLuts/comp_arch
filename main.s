7fbf1c8e7ddb1de1d0de31c559a4285452962200
Arnon Lutsky <realsplashingfish@gmail.com>
2024-02-07 21:19:36 +0200

    Update main.s

--- main.s
+++ main.s
@@ -8,11 +8,10 @@ rand_num:
     .space 8, 0x0
 seed:
     .space 8, 0x0
-range:
-    .long 10
 user_guess:
     .space 8, 0x0
-
+index_i:
+    .space 8, 0X0
 .section .rodata
 user_greet_fmt:
     .string "Enter configuration seed: "
@@ -26,8 +25,6 @@ lost_fmt:
     .string "Game over, you lost :(. The Correct answer was %d\n"
 won_fmt:
     .string "Congratz! You won! \n"
-debug:
-    .string "debug %d \n"
 
 .section .text
 .globl main
@@ -38,9 +35,6 @@ main:
     pushq %rbp
     movq %rsp, %rbp
 
-    xorq %r12, %r12
-    xorq %r13, %r13
-    xorq %r14, %r14
 
     # Print the prompt
     movq $user_greet_fmt, %rdi
@@ -53,12 +47,6 @@ main:
     xorq %rax, %rax
     call scanf
 
-        //debug print
-    movq $debug, %rdi
-    movq seed, %rsi
-    xorq %rax, %rax
-    call printf
-
     # Seed the rand function
     movq seed, %rdi
     xorq %rax, %rax
@@ -70,20 +58,13 @@ main:
     call rand
     # get the remainder after dividing by 10 (like doing rand_num%10)
     movq $10, %rcx
-    // movq $rand_num, %rdx
     xorq %rdx, %rdx
     divq %rcx
     movl %edx, rand_num
 
 
-        //debug print
-    movq $debug, %rdi
-    movq rand_num, %rsi
-    xorq %rax, %rax
-    call printf
-
     # Loop for the guessing game (up to 5 attempts)
-    movq $5, %r12  # Set loop counter to 5
+    movq $5, $index_i  # Set loop counter to 5
 
 .loop:
     # Print the prompt
@@ -97,13 +78,6 @@ main:
     xorq %rax, %rax
     call scanf
 
-
-    //debug print
-    movq $debug, %rdi
-    movq user_guess, %rsi
-    xorq %rax, %rax
-    call printf
-
     # Compare the guess with the random number
     movq rand_num, %rdi
     cmp %rdi, user_guess
@@ -114,9 +88,9 @@ main:
     xorq %rax, %rax
     call printf
 
-    decq %r12
-    
-    jnz .loop  # Jump back to the loop if attempts remaining
+    # Continue the loop if we still have remaining guesses
+    decq $index_i
+    jnz .loop 
 
 .lost:
     # Print the result if the player loses
